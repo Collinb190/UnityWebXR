f90b778cc90ef1bc7442b1abfdc096e520d90875
Lin <100497279+Collinb190@users.noreply.github.com>
2025-01-10 21:25:32 -0600

    mouse works for picking up ball

---
+++ BowlingWebXR/Assets/Scripts/DragBall.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class BallPickup : MonoBehaviour
+{
+    private Vector3 mOffset;
+    private float mZCoord;
+    private void OnMouseDown()
+    {
+        mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+        // Store offset = gameObject world pos - mouse world pos
+        mOffset = gameObject.transform.position - GetMouseWorldPos();
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPos() + mOffset;
+    }
+}
